a343730ce9357cde273925a228f72fc94ff63240
Bhavin <bhavin_1881@hotmail.com>
2020-08-06 22:16:25 +1200

    Adding the first Python file to the repository which has a function that prints two strings.

---
+++ firstPyPython.py
@@ -0,0 +1,8 @@
+"""This is the first Python file created in this Python Repository"""
+
+
+def linePrinter():
+    print("This is just a String!")
+    print("Why is Python so simple to use??")
+
+linePrinter()
