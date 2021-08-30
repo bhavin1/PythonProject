ef7e0e15dec41e06033f67aed923b878edc71871
Bhavin <bhavin_1881@hotmail.com>
2021-08-31 11:37:31 +1200

    Added some code to the PE7.py file

--- PE7.py
+++ PE7.py
@@ -3,12 +3,15 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 What is the 10 001st prime number?
 """
 
-primeCounter = 0
-inputValue = 1
+print("Please enter a number to check for prime or not: ")
+userInput = int(input())
 
-while inputValue < 10:
-    print(inputValue)
-    inputValue += 1
 
-for x in range(inputValue):
-    print(x)
+for x in range(1, userInput):
+    for y in range(1,userInput):
+        print("This loop is in y " + str(y) + " times!")
+    print("This loop is in x " + str(x) + " times!")
+#while counter <= userInput:
+    #if((userInput % counter == 0) & (userInput % 1 == 0)):
+    #    primeCounter.append(userInput)
+    #counter += 1
