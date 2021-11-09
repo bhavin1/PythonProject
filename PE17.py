135c55a79f81c088f2acac159a7d0c80d417678b
Bhavin Patel <bhavin_1881@hotmail.com>
2021-11-09 21:07:18 +1300

    Added problem number 9

--- PE17.py
+++ PE17.py
@@ -9,19 +9,42 @@ import itertools
 
 
 numberDict = {
-    '1' : "one",
-    '2' : "two",
-    '3' : "three",
-    '4' : "four",
-    '5' : "five",
-    '6' : "six",
-    '7' : "seven",
-    '8' : "eight",
-    '9' : "nine",
-    '10' : "ten"
+    1 : "one",
+    2 : "two",
+    3 : "three",
+    4 : "four",
+    5 : "five",
+    6 : "six",
+    7 : "seven",
+    8 : "eight",
+    9 : "nine",
+    10 : "ten",
+    11 : "eleven",
+    12 : "twelve",
+    13 : "thirteen",
+    14 : "fourteen",
+    15 : "fifteen",
+    16 : "sixteen",
+    17 : "seventeen",
+    18 : "eighteen",
+    19 : "nineteen",
+    20 : "twenty",
+    30 : "thirty",
+    '' : "forty",
+    '' : "fifty",
+    '' : "sixty",
+    '' : "seventy",
+    '' : "eighty",
+    '' : "ninty"
 }
 
-for x in range(1,21):
-    output = numberDict.get(str(x))
+letterSum = 0
+
+for x in range(1,31):
+    output = numberDict.get(x)
+    highNum = str(output)
+    letterSum += len(output)
     print(output + ": " + str(len(output)))
 #print(numberDict)
+
+print(letterSum)
